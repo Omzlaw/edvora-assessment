18e5b1faf23cf3741e2c401d70eee465f2e16037
Omeiza Alabi <omeizaalabi@gmail.com>
2022-02-07 16:49:53 +0100

    implement scroll on arrow click

--- components/ProductCategory.js
+++ components/ProductCategory.js
@@ -3,28 +3,31 @@ import Image from 'next/image';
 
 import ProductCard from './ProductCard';
 
+import {useRef} from 'react';
+
 
 const ProductCategory = (props) => {
+    const sliderRef = useRef(null);
     const {productName, products} = props;
     return (
         <div className={styles.container}>
             <h1 className={styles.categoryText}>{productName}</h1>
             <div className={styles.divider}></div>
             <div className={styles.productContainer}>
-            <div id="slider" className={styles.productSliderContainer}>
+            <div ref={sliderRef} className={styles.productSliderContainer}>
                     {products.map((product, index) => (<ProductCard key={index} product={product} />))}
                 </div>
                 <div className={styles.arrow}>
-                    <a onClick={(e) => scrollAction(e)}><Image src="/arrow.svg" alt="Scrolling Arrow" width={10} height={33} /></a>
+                    <a onClick={(e) => scrollAction(e, sliderRef)}><Image src="/arrow.svg" alt="Scrolling Arrow" width={10} height={33} /></a>
                 </div>
             </div>
         </div>
     );
 }
 
-const scrollAction = (e) => {
-    const productSliderContainer = document.querySelector('#slider');
-    productSliderContainer.scrollRight += 80;
+const scrollAction = (e, sliderRef) => {
+    sliderRef.current.scrollLeft += 80;
 }
+
 
 export default ProductCategory;
